e14e4ccc84de13cbbbddd4661da02f49e6f115d3
NamPhuThuy <ttnam957@gmail.com>
2025-12-06 15:20:07 +0700

    update

--- MAXAdapter.cs
+++ MAXAdapter.cs
@@ -1,8 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
+/*
+namespace NamPhuThuy.MAXAdapter
+{
+    using System;
+    using System.Collections;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.SceneManagement;
+
+    public class MAXAdapter : Singleton<MAXAdapter>
+    {
+        [Header("SCRIPTABLE OBJECT")]
+        [SerializeField] private IntVariable currentLevel;
+
+        [Header("MAX Ad Units")]
+#if UNITY_ANDROID
+        [SerializeField] private string maxSdkKey =
+            "YOUR_MAX_SDK_KEY_ANDROID";
+        [SerializeField] private string interstitialAdUnitId = "YOUR_INTER_AD_UNIT_ID_ANDROID";
+        [SerializeField] private string rewardedAdUnitId    = "YOUR_REWARDED_AD_UNIT_ID_ANDROID";
+        [SerializeField] private string bannerAdUnitId      = "YOUR_BANNER_AD_UNIT_ID_ANDROID";
+        [SerializeField] private string mrecAdUnitId        = "YOUR_MREC_AD_UNIT_ID_ANDROID";
+#elif UNITY_IOS
+        [SerializeField] private string maxSdkKey =
+            "YOUR_MAX_SDK_KEY_IOS";
+        [SerializeField] private string interstitialAdUnitId = "YOUR_INTER_AD_UNIT_ID_IOS";
+        [SerializeField] private string rewardedAdUnitId    = "YOUR_REWARDED_AD_UNIT_ID_IOS";
+        [SerializeField] private string bannerAdUnitId      = "YOUR_BANNER_AD_UNIT_ID_IOS";
+        [SerializeField] private string mrecAdUnitId        = "YOUR_MREC_AD_UNIT_ID_IOS";
+#else
+        [SerializeField] private string maxSdkKey           = "unused";
+        [SerializeField] private string interstitialAdUnitId = "unused";
+        [SerializeField] private string rewardedAdUnitId    = "unused";
+        [SerializeField] private string bannerAdUnitId      = "unused";
+        [SerializeField] private string mrecAdUnitId        = "unused";
 #endif
 
 namespace NamPhuThuy.MAXAdapter
@@ -47,40 +77,5 @@ namespace NamPhuThuy.MAXAdapter
 
         #endregion
     }
-
-    /*#if UNITY_EDITOR
-    [CustomEditor(typeof(MAXAdapter))]
-    [CanEditMultipleObjects]
-    public class MAXAdapterEditor : Editor
-    {
-        private MAXAdapter script;
-        private Texture2D frogIcon;
-        
-        private void OnEnable()
-        {
-            frogIcon = Resources.Load<Texture2D>("frog"); // no extension needed
-        }
-        
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-            script = (MAXAdapter)target;
-
-            ButtonResetValues();
-        }
-
-        private void ButtonResetValues()
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button(new GUIContent("Reset Values", frogIcon), GUILayout.Width(InspectorConst.BUTTON_WIDTH_MEDIUM)))
-            {
-                script.ResetValues();
-                EditorUtility.SetDirty(script); // Mark the object as dirty
-            }
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-        }
-    }
-    #endif*/
 }
+*/
